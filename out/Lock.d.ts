bbeacb6196bc7405aca8869c99e9999054257531
LexRiver <lexriver@gmail.com>
2024-09-25 12:46:41 +0300

    update to module

--- out/Lock.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Lock {
-    protected isLocked: boolean;
-    protected waitForUnlockAndLockAsync(msStep?: number, maxMsToWait?: number): Promise<void>;
-    protected unlockAsync(): Promise<void>;
-    lockAndExecuteAsync(action: () => Promise<void> | void, msStep?: number, maxMsToWait?: number): Promise<void>;
-}
